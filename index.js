6c32abde8cb18b65d4410cd5648ca2e3d91636b1
waynetravis <waynetavis123@gmail.com>
2025-06-15 15:40:12 +0300

    commit changes

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 function addingEventListener() {
+let input = document.getElementById('button');
+input.addEventListener('click', function () {
+alert('I was clicked!');
+});
 }
+function addingEventListener() {
+const input = document.getElementById('button');
+
+function clickAlert() {
+alert('I was clicked!');
+}
+
+input.addEventListener('click', clickAlert); }
